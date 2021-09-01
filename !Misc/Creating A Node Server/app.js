dd0bc640615b57fc5c097038c12c450d5e59ae58
st1llwater <alokpunjofficial@gmail.com>
2021-09-01 19:51:21 +0530

    Response Added

--- !Misc/Creating A Node Server/app.js
+++ !Misc/Creating A Node Server/app.js
@@ -1,8 +1,29 @@
-const http = require("http"); // HTTP Inbuilt Module Imported To Create Server
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-  // createServer Method Which Returns a Server, Takes A Function Which Runs
-  console.log(req); // Whenever A Req Is Sent. The Function Takes 2 Patameters i.e. Request and Response.
+  const url = req.url;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Form</title></head>");
+    res.write(
+      "<body><center><h1>Go On! Say Hi!</h1><form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>Send</button></center></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Server</title></head>");
+  res.write("<body><h1>Hello Written From Node.js Server</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
-server.listen(3000); // Making the Server Listen To A Port.
+server.listen(3000);
+
+// The http.createServer() method turns your computer into an HTTP server.
+
+// The http.createServer() method creates an HTTP Server object.
+
+// The HTTP Server object can listen to ports on your computer and execute a function, a requestListener, each time a request is made.
