7d76588eb597da3eedb82c9b49bb1d194954b8ce
st1llwater <alokpunjofficial@gmail.com>
2021-09-08 14:33:14 +0530

    Express

--- !Misc/Express/app.js
+++ !Misc/Express/app.js
@@ -1,19 +1,15 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In The MiddleWare!");
-  next(); // Allows The Req To Proceed To Next Middleware In line...
+app.use("/users", (req, res, next) => {
+  res.send("<h1>Middleware Handling /users</h1>");
 });
 
-app.use((req, res, next) => {
-  console.log("In Another MiddleWare!");
-  res.send("<h1>Hello From Express Side</h1>");
+app.use("/", (req, res, next) => {
+  res.send("<h1>Middleware Handling /</h1>");
 });
 
-const server = http.createServer(app);
+app.listen(3000);
 
-server.listen(3000);
+//Sequence Matters
