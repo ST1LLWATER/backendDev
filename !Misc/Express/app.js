cf1b968dc27ed632fbc4d63522ccf9f6aaac541c
st1llwater <alokpunjofficial@gmail.com>
2021-09-10 14:40:11 +0530

    Middlewares

--- !Misc/Express/app.js
+++ !Misc/Express/app.js
@@ -4,7 +4,23 @@ const app = express();
 
 app.use(bodyParser.urlencoded());
 
+const checkAdmin = (req, res, next) => {
+  if (req.query.admin === "true") {
+    res.status(200);
+    // res.json(req.query);
+    next();
+  } else {
+    res.send("You Aint Admin LMAO");
+  }
+};
+
+const sendRes = (req, res) => {
+  res.json({ query: req.query });
+};
+
 app.use(express.static("./public"));
+
+app.use("/", checkAdmin, sendRes);
 
 app.use("/addProduct", (req, res, next) => {
   console.log("Add Product Page");
@@ -15,9 +31,9 @@ app.use("/product", (req, res, next) => {
   res.redirect("/");
 });
 
-app.use("/", (req, res, next) => {
-  res.send("<h1>Welcome To Home</h1>");
-});
+// app.use("/", (req, res, next) => {
+//   res.send("<h1>Welcome To Home</h1>");
+// });
 
 app.listen(3000);
 
