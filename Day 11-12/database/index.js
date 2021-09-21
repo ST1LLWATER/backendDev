4d6a8c33a90c89952732541f089f813bf68207cc
st1llwater <alokpunjofficial@gmail.com>
2021-09-21 17:23:07 +0530

    Redis Setup

--- Day 11-12/database/index.js
+++ Day 11-12/database/index.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("postgres", "postgres", "123456789", {
+const sequelize = new Sequelize("postgres", "postgres", "12345678", {
   host: "localhost",
   dialect: "postgres",
 });
